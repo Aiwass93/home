94c5abf1c651e261837576301d1be88059d97096
Aiwass93 <88805209+Aiwass93@users.noreply.github.com>
2025-12-01 15:33:17 +0000

    Refactor term.sh comments and fix typos
    Updated comments for clarity and fixed typos.

---
+++ .local/bin/term.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# warpper script for suckless st, to show cowsay animals say your customize quotes from database
+# define whcih animals to show
+target_animal=$(shuf -e stegosaurus dragon milk turkey -n 1)
+
+# run command
+fortune ~/.local/share/fortune/lolita-database | expand | iconv -f utf-8 -t ascii//TRANSLIT | fold -s -w 60 | cowsay -n -f "$target_animal"
+
+printf "\033]2;st\007"
+
+# excute shell
+exec zsh
